bce93db0af980b5a1336cb15e2a2da5696768368
michaelplynch <89206184+michaelplynch@users.noreply.github.com>
2023-07-21 14:58:59 +0100

    update plotting function and benchmark

--- R/plot_hashtag.R
+++ R/plot_hashtag.R
@@ -2,13 +2,15 @@
 #'
 #' @param df dataframe with hashtags as columns and cells as rows
 #' @param split variable to split histogram by
+#' @param xlim z axis limits, lower defaults to zero
+#' @param ylim y axis limits, lower defaults to zero
 #' @return list of plots for each hashtag in df
 #' @export
 #' 
 #' @importFrom ggpubr gghistogram
 #' @importFrom dittoSeq dittoColors
 #' 
-plot_hashtag<-function(df, split=NULL) {
+plot_hashtag<-function(df, split=NULL,xlim=c(0,NA),ylim=c(0,NA)) {
 plots <- list()
 for (i in seq_along(colnames(df))) {
   tag<-colnames(df)[i]
@@ -22,7 +24,7 @@ for (i in seq_along(colnames(df))) {
   }
   
   
-  plotx<-gghistogram(df, x=tag, fill = fill, palette = "lancet", alpha=0.7)
+  plotx<-gghistogram(df, x=tag, fill = fill, palette = "lancet", alpha=0.7) + xlim(xlim) + ylim(ylim)
   plots[[i]]<-plotx
 }
 return(plots)
